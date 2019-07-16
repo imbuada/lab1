cb0ee9cb499cbbbeb0ec0fd5f06f767dbfd140d1
imbuada <imbuada@student.apc.edu.ph>
2019-07-16 09:24:53 +0800

    no message

--- static/js/indexScript.js
+++ static/js/indexScript.js
@@ -1,5 +1,22 @@
 function myFunction() {
     location.href = "#top";
   }
+//manual slideShow
+var slideIndex = 1;
+showDivs(slideIndex);
 
-  
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("myPics");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
