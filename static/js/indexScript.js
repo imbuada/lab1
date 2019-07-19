a102b14f0ee2f63a9b253fb4a2a0c41b569e7948
imbuada <imbuada@student.apc.edu.ph>
2019-07-19 08:58:49 +0800

    added slidewhos ;)

--- static/js/indexScript.js
+++ static/js/indexScript.js
@@ -1,4 +1,4 @@
-
+/*
 function myFunction() {
     location.href = "#top";
   }
@@ -11,6 +11,7 @@ function plusDivs(n) {
   showDivs(slideIndex += n);
 }
 
+
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("myPics");
@@ -21,6 +22,29 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+*/
+var slideIndex = [1,1];
+var slideId = ["myPics", "mhwScs"]
+showDivs(1, 0);
+showDivs(1, 1);
+
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
+
+
 /*
 var myIndex = 0;
 carousel();
